0df5de1ce5a5f7a0905a1dbae17f947baeaadd4c
llyaRaduk <llya_Raduk@mail.ru>
2021-06-14 21:47:37 +0300

    responsiv MEnu

--- js/slick.js
+++ js/slick.js
@@ -1,22 +1,87 @@
 $('.news_slider').slick({
+ 
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 1,
   dots: true,
   responsive: [
     {
-      breakpoint: 500,
+      breakpoint: 600,
       settings: {
        
         slidesToShow: 1,
       }
     },
     {
-      breakpoint: 1079,
+      breakpoint: 850,
       settings: {
        
         slidesToShow: 2,
       }
     },
+    
   ]
+  
+});
+
+$('.teachers_slider').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 770,
+      settings: {
+       
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 483,
+      settings: {
+       
+        slidesToShow: 1,
+      }
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+       
+        slidesToShow: 3,
+      }
+    },
+  ]
+  
+});
+
+$('.students_slider').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 770,
+      settings: {
+       
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 483,
+      settings: {
+       
+        slidesToShow: 1,
+      }
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+       
+        slidesToShow: 3,
+      }
+    },
+  ]
+  
 });
