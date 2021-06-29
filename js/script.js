c9c619f4692faee1d53a9c98ca4ddf03e116993c
llyaRaduk <llya_Raduk@mail.ru>
2021-06-29 23:05:51 +0300

    add some js LEFT time

--- js/script.js
+++ js/script.js
@@ -1,12 +1,47 @@
 
 
-document.querySelector('.menu__box').onclick = function(event) {
-    
-    event.target.nextSibling.nextSibling.firstChild.nextSibling.classList.toggle('open');
-  
-};
-/* let arr = document.querySelectorAll('.dropdown_content_burger');
+document.querySelector('.menu__box').onclick = function (event) {
 
-arr.forEach(function(item, i){
-    item.classList.add(i+1);
-}); */
+    event.target.nextSibling.nextSibling.firstChild.nextSibling.classList.toggle('open');
+
+};
+
+
+
+
+
+let clock = document.querySelector('.date_clock');
+
+function getCurrentTimeString() {
+    return new Date().toTimeString().replace(/ .*/, '');
+}
+
+setInterval(
+    () => clock.innerHTML = getCurrentTimeString(),
+    1000
+
+);
+
+function leftTime() {
+    let fullTimeFromDate = new Date().toTimeString().replace(/ .*/, '');
+    let hoursFromDate = parseInt(new Date().getHours());
+    let minutesFromDate = parseInt(new Date().getMinutes());
+    let arrayTegsClock = document.querySelectorAll('.timeToLessons');
+    for (let i = 0; i < arrayTegsClock.length; i++){
+        let Time = arrayTegsClock[i].innerHTML;
+        let hours = parseInt(Time.slice(0,2));
+        let minutes = parseInt(Time.slice(3,5));
+        if(hours==hoursFromDate){
+            arrayTegsClock[i].parentNode.classList.add('activeClock') ;
+        }
+    }
+}
+
+setInterval(
+    () => leftTime(),
+    10000
+);
+
+
+
+
