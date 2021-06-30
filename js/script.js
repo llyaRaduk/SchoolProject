1bcc982a342d59e993bbc5ea6f796452be8dd8e8
llyaRaduk <llya_Raduk@mail.ru>
2021-06-30 23:40:09 +0300

    finished add leftTime

--- js/script.js
+++ js/script.js
@@ -19,27 +19,48 @@ function getCurrentTimeString() {
 setInterval(
     () => clock.innerHTML = getCurrentTimeString(),
     1000
-
 );
 
 function leftTime() {
     let fullTimeFromDate = new Date().toTimeString().replace(/ .*/, '');
-    let hoursFromDate = parseInt(new Date().getHours());
-    let minutesFromDate = parseInt(new Date().getMinutes());
+    let hoursFromDate = new Date().getHours();
+    let minutesFromDate = new Date().getMinutes();
+    let checkDateTime = parseInt(hoursFromDate.toString() + minutesFromDate.toString());
     let arrayTegsClock = document.querySelectorAll('.timeToLessons');
-    for (let i = 0; i < arrayTegsClock.length; i++){
+    let arrayTegsLeftTime = document.querySelectorAll('.leftTime_item');
+    for (let i = 0; i < arrayTegsClock.length; i++) {
         let Time = arrayTegsClock[i].innerHTML;
-        let hours = parseInt(Time.slice(0,2));
-        let minutes = parseInt(Time.slice(3,5));
-        if(hours==hoursFromDate){
-            arrayTegsClock[i].parentNode.classList.add('activeClock') ;
+        let firstHours = Time.slice(0, 2);
+        let firstMinutes = Time.slice(3, 5);
+        let firstCheckTime = parseInt(firstHours + firstMinutes);
+        let lastHours = Time.slice(6, 8);
+        let lastMinutes = Time.slice(9, 11);
+        let lastCheckTime = parseInt(lastHours + lastMinutes);
+        if (checkDateTime >= firstCheckTime) {
+            if (checkDateTime<lastCheckTime) {
+                for (let i = 0; i < arrayTegsClock.length; i++) {
+                    arrayTegsClock[i].parentNode.classList.remove('activeClock')
+                    arrayTegsLeftTime[i].classList.remove('leftTime_item_active')
+                }
+                arrayTegsLeftTime[i].innerHTML= "ост. "+(60-minutesFromDate+parseInt(lastMinutes)).toString()+" мин";
+                arrayTegsClock[i].parentNode.classList.add('activeClock');
+                arrayTegsLeftTime[i].classList.add('leftTime_item_active');
+
+            }
+            if (checkDateTime==lastCheckTime) {
+                for (let i = 0; i < arrayTegsClock.length; i++) {
+                    arrayTegsClock[i].parentNode.classList.remove('activeClock')
+                    arrayTegsLeftTime[i].classList.remove('leftTime_item_active')
+                }
+            }
         }
+        
     }
 }
 
 setInterval(
     () => leftTime(),
-    10000
+    2000
 );
 
 
